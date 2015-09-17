c870039bc72d2cd4955b24e4a287da5b1e9cf4cb
Florian Keller <mail@floriankeller.de>
2015-09-17 20:46:10 +0200

    Added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Lint Task
+gulp.task('lint', function() {
+	return gulp.src(['*.js', '!config.js*'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+// Concatenate & minify JS
+gulp.task('scripts', function() {
+	return gulp.src(['*.js', '!config.js*'])
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist'))
+		.pipe(rename('all.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+	gulp.watch(['*.js', '!config.js*'], ['lint', 'scripts']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'scripts', 'watch']);
+
+gulp.on('stop', function() {
+	process.nextTick(function() {
+		process.exit(0);
+	});
+});
+
